d8f3e3be811e226b0d15707401c15d2687e9e051
Teomyr <teo@teomyr.eu>
2011-02-15 03:01:40 +0100

    Localization and text changes for the "Adjust conflicting keybindings" UI option

--- Paging/Localization-deDE.lua
+++ Paging/Localization-deDE.lua
@@ -11,6 +11,10 @@ L["Selects the most appropriate profile automatically at each login."] = "Verwen
 L["Profiles at the top of the list have priority over those at the bottom."] = "Einträge am Anfang der Liste werden gegenüber den nachfolgenden Einträgen bevorzugt.";
 L["The relevant profile is tagged with \"automatic\" in the list."] = "Das entsprechende Profil ist dabei mit \"automatisch\" gekennzeichnet.";
 
+L["Adjust key bindings for modifiers"] = "Tastenbelegung für Modifikator anpassen"
+L["Make sure that key presses involving modifiers such as CTRL, ALT or SHIFT go to the correct action buttons."] = "Stellt sicher, dass Tastendrücke mit STRG, ALT oder Umschalt der richtigen Leiste zugeordnet werden."
+L["This option is recommended unless you want to handle conflicting key bindings (e.g. with the pet bar) by yourself."] = "Diese Einstellung wird empfohlen, es sei denn Ihr möchtet Konflikte (etwa mit der Standardbelegung Eurer Begleiterleiste) selbst beheben."
+
 L["Use macro modifier syntax to specify the page numbers, separated by semicolons."] = "Benutzt die üblichen Makro-Bedingungen, um Seitenzahlen anzugeben. Trennt die Einträge mit Strichpunkten.";
 L["If a condition is missing a page number, it will use the default page instead."] = "Fehlt die Seitennummer, so verwendet Paging die Standardleiste.";
 L["Make sure the selector ends with a semicolon (%s)."] = "Der Selektor sollte mit einem Strichpunkt (%s) enden.";
--- Paging/Localization-frFR.lua
+++ Paging/Localization-frFR.lua
@@ -11,6 +11,10 @@ L["Selects the most appropriate profile automatically at each login."] = "Select
 L["Profiles at the top of the list have priority over those at the bottom."] = "Les profils du haut ont une priorité plus importante que ceux du bas.";
 L["The relevant profile is tagged with \"automatic\" in the list."] = "Le profil approprié est taggé \"automatique\" dans la liste.";
 
+L["Adjust key bindings for modifiers"] = "Ajuster les raccourcis conflictuels"
+L["Make sure that key presses involving modifiers such as CTRL, ALT or SHIFT go to the correct action buttons."] = "Toujours s'assurer que les touches modifiées par CTRL, ALT ou MAJ sont correctement assignées"
+L["This option is recommended unless you want to handle conflicting key bindings (e.g. with the pet bar) by yourself."] = "Cette option est recommendée, a moins que les conflicts de raccourcis soient traités manuellement."
+
 L["Use macro modifier syntax to specify the page numbers, separated by semicolons."] = "Utilisez une syntaxe de macro pour spécifier chaque page, séparée par des point-virgules (;).";
 L["If a condition is missing a page number, it will use the default page instead."] = "Si le numéro de page est manquant, la barre par défaut est utilisée.";
 L["Make sure the selector ends with a semicolon (%s)."] = "Faites attention a bien finir le sélecteur avec un point-virgule (%s).";
